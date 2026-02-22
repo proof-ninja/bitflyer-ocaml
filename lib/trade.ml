35b394549d55c2e7563c7aec3f610b89dd923f1f
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2026-02-22 11:16:49 +0900

    feat: :sparkles: child order id of executions

--- lib/trade.ml
+++ lib/trade.ml
@@ -22,7 +22,7 @@ let getchildorders auth product_code =
 
 type execution = {
     id : int;
-    (* "child_order_id": "JOR20150707-060559-021935", *)
+    child_order_id: string;  (* "JOR20150707-060559-021935", *)
     side: side;
     price: float;
     size: float;
@@ -34,12 +34,13 @@ type execution = {
 let execution_of_json json =
   let open Json.Util in
   let id = member "id" json |> to_int in
+  let child_order_id = member "child_order_id" json |> to_string in
   let side = member "side" json |> to_string |> Common.side_of_string in
   let price = member "price" json |> to_float in
   let size = member "size" json |> to_float in
   let exec_date = member "exec_date" json |> to_string in
   {
-    id; side; price; size; exec_date
+    id; child_order_id; side; price; size; exec_date
   }
 
 let executions_of_json json =
